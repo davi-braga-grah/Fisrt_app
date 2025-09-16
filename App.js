fdc3f20385c0c28f2f17580fecc4e1b235d70d41
Davi_Grah <davigrah2010@gmail.com>
2025-09-16 16:56:52 -0300

    fixed

--- App.js
+++ App.js
@@ -11,29 +11,31 @@ export default function App() {
       end={{ x: 1, y: 1 }}
       style={styles.container}>
 
-      <View style={styles.container}>
+      <View style={{ flex: 1, alignItems: 'center' }}>
         <ImageBackground
           source={{ uri: 'https://triviapw.com.br/wp-content/uploads/2025/07/Zenless-Zone-Zero.webp' }}
-          style={{ width: 500, height: 500 }}>
+          style={{ width: 500, height: 500 }}
+          imageStyle={{ borderRadius: 8 }}>
         </ImageBackground>
+
         <Text style={styles.texto}>Zenless Zone Zero</Text>
 
-        <TouchableOpacity
-          style={styles.botao}
-          color="lightgray"
-          title="Like"
-          onPress={() => alert('Você curtiu!')}>
-          <Text>Like</Text>
-        </TouchableOpacity>
+        <View style={styles.container}>
+          <TouchableOpacity
+            style={styles.botao}
+            color="lightgray"
+            onPress={() => alert('Baixando....')}>
+            <Text>Baixar</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity
-          style={styles.botao}
-          color="lightgray"
-          title="Salvar"
-          onPress={() => alert('Você Salvou!')}>
-          <Text>Salvar</Text>
-        </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.botao}
+            color="lightgray"
+            onPress={() => alert('Você Salvou!')}>
+            <Text>Salvar</Text>
+          </TouchableOpacity>
 
+        </View>
       </View>
 
     </LinearGradient>
@@ -42,9 +44,8 @@ export default function App() {
 }
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
     padding: 15,
   },
   texto: {
@@ -53,18 +54,19 @@ const styles = StyleSheet.create({
     fontSize: 50,
   },
   botao: {
-    backgroundColor: '#bcbcbc',
+    backgroundColor: 'white',
     borderRadius: 5,
-    justifyContent:"center",
-    alignItens:'center',
-    shadowColor:'black',
+    justifyContent: "center",
+    alignItens: 'center',
+    shadowColor: 'black',
     shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
+    shadowOpacity: 1,
+    shadowRadius: 3.4,
+    marginHorizontal: 8,
   },
   textoBotao: {
     color: 'black',
     fontSize: 18,
     fontWeight: 'bold'
-  }
+  },
 });
